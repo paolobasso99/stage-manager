9aa30c530bf39b4947fef641e852bd69811dc5e6
Paolo <paolo.basso99@gmail.com>
2017-05-25 15:34:19 +0200

    Config ldap

--- routes/web.php
+++ routes/web.php
@@ -14,3 +14,11 @@
 Voyager::routes();
 
 Route::get('/', 'DashboardController@index')->name('voyager.dashboard');
+
+//Dummy route
+use Adldap\Laravel\Facades\Adldap;
+
+Route::get('dummy', function(){
+    //dd(config('adldap'));
+    dd(Adldap::search()->users()->get());
+});
