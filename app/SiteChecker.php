374e3378ba0320d39d1c1af357ccf54f765409f3
Paolo <paolo.basso99@gmail.com>
2017-06-01 17:34:47 +0200

    Delegating work from Site model

--- app/SiteChecker.php
+++ app/SiteChecker.php
@@ -7,11 +7,45 @@ use GuzzleHttp\Client;
 use GuzzleHttp\TransferStats;
 
 use App;
+use App\ResponseProcessor;
 
 use Carbon\Carbon;
 
 class SiteChecker
 {
+    //Chek one site
+    public function checkOne(Site $site, Client $client)
+    {
+        //Update checked_at
+        $site->checked_at = Carbon::now();
+
+        try {
+
+            //Use the Guzzle client to perform a GET request
+            $client->getAsync($site->url, [
+
+                //Set client options
+                'allow_redirects' => config('check.guzzle.allow_redirects'),
+                'connect_timeout' => config('check.guzzle.connect_timeout'),
+                'headers' => [
+                    'User-Agent' => config('check.guzzle.user_agent')
+                ],
+
+                //Perform request
+                'on_stats' => function (TransferStats $statistics) use ($site) {
+
+                    (new ResponseProcessor($site))->process($statistics);
+                    
+                }
+
+            ]);
+
+        } catch (GuzzleException $e) {
+            //
+        }
+
+    }
+
     //Check the sites that needs to be check
     public function check()
     {
@@ -34,37 +68,6 @@ class SiteChecker
 
         foreach ($sites as $site) {
             $this->checkOne($site, $client);
-        }
-
-    }
-
-    //Chek one site
-    public function checkOne(Site $site, Client $client)
-    {
-        //Update checked_at
-        $site->checked_at = Carbon::now();
-
-        try {
-
-            //Use the Guzzle client to perform a GET request
-            $client->getAsync($site->url, [
-
-                //Set client options
-                'allow_redirects' => config('check.guzzle.allow_redirects'),
-                'connect_timeout' => config('check.guzzle.connect_timeout'),
-                'headers' => [
-                    'User-Agent' => config('check.guzzle.user_agent')
-                ],
-
-                //Perform request
-                'on_stats' => function (TransferStats $statistics) use ($site) {
-                    $site->processResponse($statistics);
-                }
-
-            ]);
-
-        } catch (GuzzleException $e) {
-            //
         }
 
     }
